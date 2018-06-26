6e4631487bee31e5b922f6a2cf3c9ddf7f7661d5
Your Name <you@example.com>
2018-06-26 13:40:29 -0300

    Implementado Cadastro e Logi

--- atividades/forms.py
+++ atividades/forms.py
@@ -1,16 +1,24 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Nome'}),max_length=30, required=True, help_text='*')
     last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Sobrenome'}),max_length=30, required=True, help_text='*')
-    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder':'Email'}),max_length=254, help_text='Informe um Email válido', required=True)
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Senha'}), required=True, min_length=8, max_length=16)
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Repita a Senha'}), required=True, min_length=8, max_length=16)
+    username = forms.EmailField(widget=forms.TextInput(attrs={'placeholder':'Email'}),max_length=254, help_text='Informe um Email válido', required=True)
 
 
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'username', 'password1', 'password2')
+
+
+class LogInForm(AuthenticationForm):
+    username = forms.EmailField(widget=forms.TextInput(attrs={'placeholder':'Email'}),max_length=254, help_text='Informe um Email válido', required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
--- atividades/views.py
+++ atividades/views.py
@@ -1,21 +1,48 @@
-from django.contrib.auth import login, authenticate
-from django.shortcuts import render, redirect
 
-from atividades.forms import SignUpForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response, render
+from django.contrib.auth import login, authenticate
+from django.views.generic import FormView
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+
+from atividades.forms import SignUpForm, LogInForm
 
 # Create your views here.
 
-def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('login')
-        
-    else:
-        form = SignUpForm()
-    return render(request, 'atividades/cadastro.html', {'form':form})
+class SignUp(FormView):
+    template_name = 'atividades/cadastro.html'
+    form_class = SignUpForm
+    success_url = reverse_lazy('logou')
+
+    def form_valid(self, form):
+        aluno = form.save(commit = False)
+        aluno.save()
+        messages.success(self.request, "Aluno cadastrado com sucesso")
+        return HttpResponseRedirect(self.get_success_url())
+    
+    def form_invalid(self, form):
+        messages.error(self.request, "Por favor, preencha corretamente os campos")
+        return self.render_to_response(self.get_context_data(form = form))
+
+
+class LogIn(FormView):
+    template_name = 'atividades/login.html'
+    form_class = LogInForm
+    success_url = reverse_lazy('logou')
+
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None and user.is_active:
+            login(self.request, user)
+            return super(LogIn, self).form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+
+def logou(request):
+    return render(request, 'atividades/logou.html')
